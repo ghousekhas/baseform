be8c87f3d1a8f91ba94117b80db4e262fa9ac5de
ghousekhas <ghouse.khasim1@gmail.com>
2020-09-11 15:22:17 +0530

    Updates

---
+++ precache-manifest.d90a49d2f60d798ef353f8c7b6b4551b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747787e81fa2f55a7e69ad5e0d6f062c",
+    "url": "/baseform/index.html"
+  },
+  {
+    "revision": "cb7cf6c805e791f883ee",
+    "url": "/baseform/static/css/main.1fbcfeba.chunk.css"
+  },
+  {
+    "revision": "07dbd49db04c27ffd0f2",
+    "url": "/baseform/static/js/2.a7421fe2.chunk.js"
+  },
+  {
+    "revision": "177dce6ef9045384a932287ff9d95cbd",
+    "url": "/baseform/static/js/2.a7421fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7cf6c805e791f883ee",
+    "url": "/baseform/static/js/main.f3ea7ba9.chunk.js"
+  },
+  {
+    "revision": "a9892dc85dfa8c6cbd6f",
+    "url": "/baseform/static/js/runtime-main.c1324e80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/baseform/static/media/logo.5d5d9eef.svg"
+  }
+]);
